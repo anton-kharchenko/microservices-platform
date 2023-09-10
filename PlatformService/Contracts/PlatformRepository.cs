823aa827b441081686af9282061e12686458e7d5
Anton Kharchenko <anton.kharchenko.1997@gmail.com>
2023-09-10 13:50:18 +0300

    feat(platform): platform repository

---
+++ PlatformService/Contracts/PlatformRepository.cs
@@ -0,0 +1,27 @@
+﻿using PlatformService.Models;
+using PlatformService.Repositories;
+
+namespace PlatformService.Contracts;
+
+public class PlatformRepository : IPlatformRepository
+{
+    public bool SaveChanges()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Platform> GetAllPlatforms()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Platform GetPlatformByIf(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CreatePlatform(Platform platform)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ PlatformService/Repositories/IPlatformRepository.cs
@@ -0,0 +1,11 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Repositories;
+
+public interface IPlatformRepository
+{
+    bool SaveChanges();
+    IEnumerable<Platform> GetAllPlatforms();
+    Platform GetPlatformByIf(string id);
+    void CreatePlatform(Platform platform);
+}
