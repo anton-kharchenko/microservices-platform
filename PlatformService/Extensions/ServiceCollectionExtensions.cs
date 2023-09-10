b81caa06f0ca7099cda4357d8e5eec376f395ba2
Anton Kharchenko <anton.kharchenko.1997@gmail.com>
2023-09-10 14:03:41 +0300

    refactor(platform): create extenstion class for collection all aditional services in one place

---
+++ PlatformService/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Contracts;
+using PlatformService.Data;
+using PlatformService.Repositories;
+
+namespace PlatformService.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IPlatformRepository, PlatformRepository>();
+    }
+
+    public static void AddDb(this IServiceCollection services)
+    {
+        services.AddDbContext<AppDbContext>(op =>
+            op.UseInMemoryDatabase("InMem"));
+    }
+}
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,20 +1,15 @@
-using Microsoft.EntityFrameworkCore;
-using PlatformService.Data;
+using PlatformService.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(op =>
-    op.UseInMemoryDatabase("InMem"));
+builder.Services.AddDb();
+builder.Services.AddRepositories();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -22,9 +17,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
